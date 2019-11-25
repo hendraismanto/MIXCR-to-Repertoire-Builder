581bd0668c875d56169eac5bbda2ccf2f8c2adf9
Hendra Saputra Ismanto <41713316+hendraismanto@users.noreply.github.com>
2019-11-25 14:19:32 +0900

    Update Data table to FASTA.py

--- Data table to FASTA.py
+++ Data table to FASTA.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import glob
+import os
 
-#import data table from Data Pre-processing.py
+filenames = []
+for val in glob.glob('*full_clones_preprocess.txt'):
+    file = os.path.splitext(val)[0]
+    filenames.append(file)
 
-#convert data table to FASTA
-nona_brca_19 = brca_19.fillna('')
-f = open("brca_19_output.txt", "w")
-for index, row in nona_brca_19.iterrows():
-    f.write('>' + 'brca_19' + ' | ' + str(row['cloneId']) + ' | ' + str(row['cloneCount']) + ' | ' + row['allVHitsWithScore'] + ' | ' + row['allDHitsWithScore'] + ' | ' + row['allJHitsWithScore'] + ' | ' + row['allCHitsWithScore'] + '\n' + row['full ig'] + '\n')
-f.close()
-#these code still include all heavy and light chain
-#need to seperate heavy from light chain
+for filename in filenames:
+    data = pd.read_csv(filename + '.txt', delimiter = '\t')
+    heavy = data.loc[data['allVHitsWithScore'].str.contains('IGHV')]
+    name = filename.split('_full')
+    
+    nona_data = heavy.fillna('')
+    f = open(filename + '_heavy_chain_AA.txt', 'w')
+    for index, row in nona_data.iterrows():
+        f.write('>' + name[0] + '_' + str(row['cloneId']) + ' | ' + str(row['cloneCount']) + ' | ' + row['allVHitsWithScore'] + ' | ' + row['allDHitsWithScore'] + ' | ' + row['allJHitsWithScore'] + ' | ' + row['allCHitsWithScore'] + '\n' + row['full ig'] + '\n')
+    f.close()

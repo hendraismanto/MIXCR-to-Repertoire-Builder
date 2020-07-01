e61fe7d5c45ee297622e98b4c5d024365174753e
Hendra Saputra Ismanto <41713316+hendraismanto@users.noreply.github.com>
2020-07-01 17:50:48 +0900

    Update MiXCR_to_FASTA_filtered_seqs.py

--- MiXCR_to_FASTA_filtered_seqs.py
+++ MiXCR_to_FASTA_filtered_seqs.py
@@ -37,3 +37,30 @@ def import_pseudo_to_fasta(cohort, filename):
         for index, row in data.iterrows():
             f.write('>' + cohort + '_' + filename + '_' + str(row['cloneId']) + '\n' + row['pseudo ig'] + '\n')
             
+def main():
+
+    parser = argparse.ArgumentParser(description = 'change MiXCR output into fasta for human and mouse aa/nt')
+
+    parser.add_argument('-i', dest = 'input_file', help = 'Input file (tsv)')
+    parser.add_argument('-s', dest = 'species', default = 'human', help = 'Species (human or mouse) default = human')
+    parser.add_argument('-t', dest = 'type_job', default = 'all', help = 'type of job (aa+nt or aa or nt), default = aa&nt')
+    parser.add_argument('-o', dest = 'out_dir', help = 'directory of output')
+    parser.add_argument('-p', dest = 'pseudo_aa', default = None, help = 'generate only pseudo aa seq (keyword = pseudo)')
+    args = parser.parse_args()
+    
+    os.makedirs(args.out_dir, exist_ok=True)
+
+    filename = os.path.abspath(args.input_file)
+    filename_ = os.path.basename(filename)
+    filename_only, file_extension = os.path.splitext(filename_)
+    
+
+    logging.basicConfig(filename=os.path.join(args.out_dir, "out.log"), level=logging.INFO)
+    logger = logging.getLogger()
+    sh = logging.StreamHandler()
+    logger.addHandler(sh)
+    
+    logging.info("species: {}".format(args.species))
+    logging.info('type of job: {}'.format(args.type_job))
+    logging.info("out_dir: {}".format(os.path.abspath(args.out_dir)))
+    
